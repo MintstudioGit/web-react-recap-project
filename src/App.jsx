3524564239de770f1ff1e3d2bfffc70bb08b66f2
MintstudioGit <alainkolomoni@gmail.com>
2024-06-05 12:33:57 +0200

    Ex2and3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
+/// src/App.jsx
+import React, { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm/ColorForm";
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  const addColor = (newColor) => {
+    setColors([newColor, ...colors]);
+  };
+
+  const deleteColor = (id) => {
+    setColors(colors.filter((color) => color.id !== id));
+  };
+
   return (
     <>
       <h1>Theme Creator</h1>
-
-      {initialColors.map((color) => {
-        return <Color key={color.id} color={color} />;
-      })}
+      <ColorForm addColor={addColor} />
+      <div className="flex-container">
+        {colors.map((color) => {
+          return (
+            <Color
+              key={color.id}
+              color={color}
+              deleteColor={deleteColor}
+              updateColor={updateColor}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,6 +1,8 @@
 import "./Color.css";
+import "../ColorForm/ColorForm";
+import "../ColorInput/ColorInput";
 
-export default function Color({ color }) {
+export default function Color({ color, deleteColor }) {
   return (
     <div
       className="color-card"
@@ -12,6 +14,9 @@ export default function Color({ color }) {
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
+      <button className="delete-button" onClick={() => deleteColor(color.id)}>
+        Delete
+      </button>
     </div>
   );
 }
